1c84339aaa19f0512091c8f56bbad37d643c1f05
SeongMinKwon <sleepingsm813@gmail.com>
2022-05-03 19:25:31 +0900

    220412 92334 KAKAO BLIND 신고 결과 받기 문제

--- 2022_Kakao Blind Recruitment/KSM/92334.py
+++ 2022_Kakao Blind Recruitment/KSM/92334.py
@@ -1,11 +1,14 @@
 # 2022 KAKAO BLIND RECRUITMENT 신고 결과 받기
 
-# def BiggerThanK(id_list, report_list, k):
-#     for x, y in a:
-
+# list.count(a) 는 list 안의 a 요소의 개수 구하는 방법
 
 def solution(id_list, report, k):
     answer = []
-    test = list(sorted(set(map(str, (tmp.split() for tmp in report)))))
-
+    s1 = [s.split()[1] for s in set(report)]
+    print(s1)
+    s3 = [s2 for s2 in set(s1) if s1.count(s2) >= k]
+    print(s3)
+    s4 = [s.split()[0] for s in set(report) if s.split()[1] in s3]
+    print(s4)
+    answer = [s4.count(i) for i in id_list]
     return answer
